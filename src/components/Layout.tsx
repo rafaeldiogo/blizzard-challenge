0f2ebaa3c765f34f196c93303da4054663766574
rafaeldiogo <devrafaeldiogo@gmail.com>
2024-06-11 11:44:25 -0300

    refactor: diabloiv background directory

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 
 const Layout = () => {
   return (
-    <div className="bg-cover bg-[url('./assets/bg-banner.png')]">
+    <div className="bg-cover bg-[url('./assets/bannerhero/images/diabloiv-bg.png')] h-[736px]">
       <Menu />
       <BannerHero />
     </div>
