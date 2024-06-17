1807b8bee1cf5ebb37ef3652a83b5742d16f5f5b
rafaeldiogo <devrafaeldiogo@gmail.com>
2024-06-17 10:58:33 -0300

    feat: create icons and banners object

---
+++ src/utils/mapBannerHeroes.ts
@@ -0,0 +1,47 @@
+import logoDiabloIVLarge from "../assets/bannerhero/images/diabloiv-logo.png";
+import logoHearthstoneLarge from "../assets/bannerhero/images/hearthstone-logo.png";
+import logoWowLarge from "../assets/bannerhero/images/wow-logo.png";
+
+import logoPreviewDiabloIV from "../assets/bannerhero/images/diablo-animation-cover.png";
+import logoPreviewHearthstone from "../assets/bannerhero/images/hearthstone-animation-cover.png";
+import logoPreviewWow from "../assets/bannerhero/images/wow-animation-cover.png";
+
+import gifDiabloIV from "../assets/bannerhero/images/diablo-animation.gif";
+import gifHearthstone from "../assets/bannerhero/images/hearthstone-animation.gif";
+import gifWow from "../assets/bannerhero/images/wow-animation.gif";
+
+export const mapBannerHeroes = [
+  {
+    title: "Retorne à escuridão com o game Diablo IV",
+    subtitle: "O retorno de Lilith traz uma era de escuridão e sofrimento",
+    textButton: "Jogar agora",
+    icon: logoDiabloIVLarge,
+    previewGif: logoPreviewDiabloIV,
+    altPreviewGif: "diablo-animation-cover",
+    gif: gifDiabloIV,
+    altGif: "diablo-animation.gif",
+    alt: "diabloiv-icon",
+  },
+  {
+    title: "Novo pacote de expansão de Hearthstone",
+    subtitle: "A Horda e a Aliança se encontraram no Vale Alterac para lutar",
+    textButton: "Reserve agora na pré-venda",
+    previewGif: logoPreviewHearthstone,
+    altPreviewGif: "hearthstone-animation-cover",
+    gif: gifHearthstone,
+    altGif: "hearthstone-animation",
+    icon: logoHearthstoneLarge,
+    alt: "hearthstone-icon",
+  },
+  {
+    title: "Desbrave as Terras Sombrias em Shadowlands! ",
+    subtitle: "O que jaz além do mundo que você conhece?",
+    textButton: "Reserve agora na pré-venda",
+    previewGif: logoPreviewWow,
+    altPreviewGif: "wow-animation-cover",
+    gif: gifWow,
+    altGif: "wow-animation",
+    icon: logoWowLarge,
+    alt: "wow-icon",
+  },
+];
---
+++ src/utils/mapGamesIcon.ts
@@ -0,0 +1,33 @@
+import iconDiabloIV from "../assets/bannerhero/icons/diabloiv.png";
+import iconHearthstone from "../assets/bannerhero/icons/hearthstone.png";
+import iconWow from "../assets/bannerhero/icons/wow.png";
+import iconDiablo from "../assets/bannerhero/icons/diablo.png";
+import iconStarCraft from "../assets/bannerhero/icons/starcraft.png";
+
+export const mapGamesIcon = [
+  {
+    icon: iconDiabloIV,
+    alt: "diabloiv-icon",
+    disabled: false,
+  },
+  {
+    icon: iconHearthstone,
+    alt: "hearthstone-icon",
+    disabled: false,
+  },
+  {
+    icon: iconWow,
+    alt: "wow-icon",
+    disabled: false,
+  },
+  {
+    icon: iconDiablo,
+    alt: "diablo-logo",
+    disabled: true,
+  },
+  {
+    icon: iconStarCraft,
+    alt: "starcraft-logo",
+    disabled: true,
+  },
+];
